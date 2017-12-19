58fe85dae25be8ed23f5cdf6c56f6891da4270bf
attachesoftware\StevenG <steveng@attachesoftware.com>
2017-12-20 10:39:21 +1100

    3.3  Configuring a Full-stack JavaScript Environment: Client Side

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const config = {
+  entry: {
+    app: ['babel-polyfill', './lib/components/Index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
+
+
+
+module.exports = config;
